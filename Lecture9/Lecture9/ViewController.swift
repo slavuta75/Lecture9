ebfbdb881e7bc994b7d22dab35b9bf408774f40c
Slavuta75 <slavuta75@gmail.com>
2018-05-25 19:27:01 +0300

    Beginning

---
+++ Lecture9/Lecture9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lecture9
+//
+//  Created by Slava Utyuzh on 25.05.2018.
+//  Copyright © 2018 Slava Utyuzh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
